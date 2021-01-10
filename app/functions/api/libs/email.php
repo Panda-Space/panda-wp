a65be5c97af9de5520c8edef86347f9e494a98a1
pincelkey <deckadecklantec@gmail.com>
2021-01-10 14:56:19 -0500

    Update | Fix | Emamil lib + Preset image

--- app/functions/api/libs/email.php
+++ app/functions/api/libs/email.php
@@ -1,6 +1,6 @@
 <?php
 
-function __getEmailTemplate($file, $data){
+function __getEmailTemplate($file, $data = null){
     ob_start(); require(__DIR__ . "/../emails/{$file}.php");
 
     return ob_get_clean();
